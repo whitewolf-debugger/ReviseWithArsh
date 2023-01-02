fb4d23b6d1f769c23283f06b5d003fe6a380bc4b
Pankaj Goel <89847876+whitewolf-debugger@users.noreply.github.com>
2023-01-02 12:53:59 +0530

    Create Minimum Deletions to Make Array Divisible.cpp

---
+++ Microsoft/Minimum Deletions to Make Array Divisible.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int minOperations(vector<int>& nums, vector<int>& numsDivide) {
+        sort(nums.begin(), nums.end());
+        int n = numsDivide.size();
+        int g = numsDivide[0];
+        
+        for(int i =1; i < n; i++) {
+            g = __gcd(numsDivide[i] , g);
+        }
+
+        for(int i = 0; i < nums.size(); i++) {
+            if(g % nums[i] == 0) return i;
+        }
+
+        return -1;
+    }
+};
